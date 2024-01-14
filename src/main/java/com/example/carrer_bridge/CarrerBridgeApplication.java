8a813cab64e1307fe2ff5d8213b41270cd148c31
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2024-01-14 22:39:52 +0100

    init project

---
+++ src/main/java/com/example/carrer_bridge/CarrerBridgeApplication.java
@@ -0,0 +1,13 @@
+package com.example.carrer_bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerBridgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrerBridgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/carrer_bridge/CarrerBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carrer_bridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
